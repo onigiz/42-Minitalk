7417b4fb8d9a5f4b121ce20536bb7c4ec1de36fb
OnurNigiz <onurnigiz@hotmail.com>
2023-08-01 14:48:27 +0300

    norm

---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: onigiz <onigiz@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/01 14:47:59 by onigiz            #+#    #+#             */
+/*   Updated: 2023/08/01 14:48:01 by onigiz           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "ft_printf/ft_printf.h"
+# include "libft/libft.h"
+
+#endif
